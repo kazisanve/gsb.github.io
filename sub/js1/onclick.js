94435408828974e62122a80dd4dcdb5bed96fbb8
kazisanve <67918316+kazisanve@users.noreply.github.com>
2020-07-06 10:07:12 -0700

    new

---
+++ sub/js1/onclick.js
@@ -0,0 +1,3 @@
+	function sanve(){
+	alert("The Feature You Had Click That is Under Development.This Feature is Comming Soon!");
+	}
---
+++ sub/js1/script.js
@@ -0,0 +1,8 @@
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 4,
+      spaceBetween: 28,
+      pagination: {
+        //el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
